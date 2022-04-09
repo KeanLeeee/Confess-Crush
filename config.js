109a0b43b15258ccd05eeef72a64403416154505
KeanLeeee <102942862+KeanLeeee@users.noreply.github.com>
2022-04-09 15:16:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Hello cô gái hay zợn zỗi zà khóc nhòe",
+    introTitle: 'Bảo Ngọc',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
