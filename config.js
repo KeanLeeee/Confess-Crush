6ec335d1538b3c624d93eac33e9915eed752fd38
KeanLeeee <102942862+KeanLeeee@users.noreply.github.com>
2022-04-09 15:37:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,17 @@
 const CONFIG = {
     titleWeb: "Gửi cô gái hay zợn zỗi và khóc nhòe",
     introTitle: 'Ngọc',
-    introDesc: `
-    à thương`,
+    introDesc: ` Chúng mình đã dành một quãng thời gian
+    cho nhau,nhưng sao chỉ vì những điều nhỏ nhặt mà đánh mất nhau.
+    Tại sao phải suy xét đúng sai? Khi trái tim chẳng bao giờ nằm bên phải.`,
     btnIntro: 'Ấn đây Ngọc ơi^^',
     title: 'Phải chăng Ngọc đang nhớ tui 🥰',
-    desc: 'Phải chăng Ngọc đang muốn tui rủ Ngọc i chơi ',
+    desc: 'Phải chăng Ngọc đang muốn tui rủ Ngọc ik chơi ',
     btnYes: 'Đúm zạiiii <3',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại muốn đi chơi với mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'hihi đương nhiên là đi với Dương đẹp trai là xiu dzui ',
+    btnReply: 'Gửi cho Dương <3',
+    reply: 'hihi đưn nhin là đi với Dương đẹptrai là xiu dzui ',
     mess: 'Mình biết mà 🥰. Mơn bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, bạn muốn đi dạo, chợ đêm hay chè 68 nè?.',
     btnAccept: 'Okiiiii lun <3',
