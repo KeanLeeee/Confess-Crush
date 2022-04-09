f476d7cc157deaddcd82a7d279ce13bb666f95ad
KeanLeeee <102942862+KeanLeeee@users.noreply.github.com>
2022-04-09 15:30:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,15 +6,15 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'Ấn đây Ngọc ơi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <3',
+    title: 'Phải chăng Ngọc đang nhớ tui 🥰',
+    desc: 'Phải chăng Ngọc đang muốn tui rủ Ngọc i chơi ',
+    btnYes: 'Đúm zạiiii <3',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại muốn đi chơi với mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'hihi đương nhiên là đi với Dương đẹp trai là xiu dzui ',
     mess: 'Mình biết mà 🥰. Mơn bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, bạn muốn đi dạo, chợ đêm hay chè 68?.',
+    messDesc: 'Tối nay 7h, bạn muốn đi dạo, chợ đêm hay chè 68 nè?.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: ''https://keanleeee.github.io/Confess-Crush/ //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: ''https://keanleeee.github.io/Confess-Crush/'' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
